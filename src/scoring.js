53ca650c6318040fc162f927a6d21752c638290d
Immodal <kychin.n@gmail.com>
2020-08-07 18:21:41 +1000

    Scoring docs

--- src/scoring.js
+++ src/scoring.js
@@ -11,18 +11,20 @@ const Scoring = {
     for(let j=stack.length-1; j>=0; j--) {
       let nFilled = 0
       for(let i=stack[j].length-1; i>=0; i--) {
+        // nFilled
+        if (stack[j][i]!=null) nFilled += 1
         // Holes
-        if (stack[j][i]==null && !emptySet.has(i, j)) nHoles += 1
-        // Blocked
+        else if (stack[j][i]==null && !emptySet.has(i, j)) nHoles += 1
+        // Blocked & tallColumns
         else if (stack[j][i]==null) {
           let blocked = Scoring.countBlocked(i, j, stack)
-          nBlocked += blocked
           if (blocked>0) tallColumns.set(j, tallColumns.has(j) ? tallColumns.get(j)+1 : 1)
+          nBlocked += blocked
         }
-        // Filled
-        else if (stack[j][i]!=null) nFilled += 1
       }
+      // Early Exit
       if (nFilled == 0) break
+      // Count rows that will be removed
       else if (nFilled == stack[j].length) nFilledRows += 1
       stackHeight += 1
     }
@@ -40,6 +42,11 @@ const Scoring = {
     return 2*nHoles + 2*nBlocked + Math.max(0, (nTallColumns-1)*10) + stackHeight + tallStackPenalty2*10 - nFilledRows
   },
 
+  /**
+   * Count the number of blocks that are blocking line of sight of this column to the top of the playfield.
+   * @param {int} i Column index to start counting from
+   * @param {int} j Row index to start counting from, non inclusive
+   */
   countBlocked: (i, j, stack) => {
     let nBlocked = 0
     for(let j2=j-1; j2>=0; j2--) {
@@ -50,6 +57,9 @@ const Scoring = {
     return nBlocked
   },
 
+  /**
+   * A Set that encodes coordinates to String for quick comparisons.
+   */
   PosSet: () => {
     const cs = {}
 
@@ -61,6 +71,11 @@ const Scoring = {
     return cs
   },
 
+  /**
+   * Returns a PosSet of all cells in stack accessible from [i,j] via BFS.
+   * @param {int} i Column index to start searching from
+   * @param {int} j Row index to start searching from
+   */
   getEmptyPostions: (i, j, stack) => {
     const origin = Tetromino.Node(i, j)
     const open = [[null, origin]]
@@ -84,10 +99,16 @@ const Scoring = {
     return closed
   },
 
+  /**
+   * Returns an array of candidate endpoints based on the given piece and stack
+   * @param {Tetromino} piece The piece to add to the stack
+   * @param {Array} stack 2D array that keeps track of Tetromino segments that have been locked in place.
+   */
   getEndPoints: (piece, stack) => {
     let endPoints = []
     for(let r=0; r<piece.rotations.length; r++) {
-      for(let i=-2; i<stack[0].length; i++) {
+      // -2 to account for inconsistent column spacing in base Tetrominoes
+      for(let i=-2; i<stack[0].length; i++) { 
         let p = piece.cons(i, 0, r)
         let ghost = Game.getGhost(p, stack)
         if (ghost != null) endPoints.push(ghost)
@@ -96,6 +117,11 @@ const Scoring = {
     return endPoints
   },
 
+  /**
+   * Returns an array where index 0 is the top score and index 1 is the top scoring endpoint.
+   * @param {Array} endPoints Array of candidate endpoints to be added to stack and scored
+   * @param {Array} stack 2D array that keeps track of Tetromino segments that have been locked in place.
+   */
   getBestEndpoint: (endPoints, stack) => {
     let minScore = 99999
     let minPiece = null 
